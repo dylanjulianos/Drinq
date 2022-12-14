5de6c82f98767972ecf22d4b2ad043f80af8e518
dylanjulianos <103484220+dylanjulianos@users.noreply.github.com>
2022-12-14 16:57:40 +0700

    feat: First commit

--- Drinq/ViewController.swift
+++ Drinq/ViewController.swift
@@ -8,10 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //MARK: - Properties
+    
+    var label: UILabel = {
+        var label: UILabel = UILabel()
+        label.text = "Hello world"
+        label.textColor = .green
+        return label
+    }()
+    
+    //MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+//        view.backgroundColor = .blue
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(label)
+        
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
     }
 
 
